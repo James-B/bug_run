00f239a72bba4a73616aacbf243d68ca86134d4e
James Blinkhorn <jblinkhorn@gmail.com>
2014-04-13 21:15:00 -0400

    Update buttons to work with new quintus code.

--- app/scripts/components.js
+++ app/scripts/components.js
@@ -1,28 +1,27 @@
+
 Q.UI.HelpButton = Q.UI.Button.extend("UI.HelpButton", {
-  init: function(p) {
-    var _this = this;
-    this._super(p, {
+  init: function(p, callback, defaultProps) {
+    this._super(Q._defaults(p,{
       x: 0,
       y: 0,
-      type: Q.SPRITE_UI | Q.SPRITE_DEFAULT,
       sheet: "ui_help_on"
-    });
-    return this.on('click', function() {
+    }));
+    
+    this.on('click', function() {
       Q.Game.stageHelpScreen();
     });
   }
 }); 
 
 Q.UI.ShareButton = Q.UI.Button.extend("UI.ShareButton", {
-  init: function(p) {
-    var _this = this;
-    this._super(p, {
+  init: function(p, callback, defaultProps) {
+    this._super(Q._defaults(p,{
       x: 0,
       y: 0,
-      type: Q.SPRITE_UI | Q.SPRITE_DEFAULT,
       sheet: "ui_share_on"
-    });
-    return this.on('click', function() {
+    }));
+
+    this.on('click', function() {
       FB.ui({method: 'apprequests',
         title: 'Bug Run',
         message: 'Play Byg Run based on the new book by Meredith Blinkhorn!',
@@ -32,15 +31,14 @@ Q.UI.ShareButton = Q.UI.Button.extend("UI.ShareButton", {
 }); 
 
 Q.UI.FBButton = Q.UI.Button.extend("UI.FBButton", {
-  init: function(p) {
-    var _this = this;
-    this._super(p, {
+  init: function(p, callback, defaultProps) {
+    this._super(Q._defaults(p,{
       x: 0,
       y: 0,
       type: Q.SPRITE_UI | Q.SPRITE_DEFAULT,
       sheet: "ui_fb_connect",
       keyActionName: "login"
-    });
+    }));
 
     if (Q.Game.User.loggedIn) {
       this.p.sheet = "ui_fb_logout";
@@ -48,15 +46,12 @@ Q.UI.FBButton = Q.UI.Button.extend("UI.FBButton", {
       this.p.sheet = "ui_fb_connect";
     }
 
-    return this.on('click', function() {
-      console.log(Q.Game.User.loggedIn);
-    if (Q.Game.User.loggedIn) {
-  //    _this.p.sheet = "ui_fb_logout";
-      Q.Game.User.disconnect();
-    } else {
-   //   _this.p.sheet = "ui_fb_connect";
-      Q.Game.User.connect();
-    }
+    this.on('click', function() {
+      if (Q.Game.User.loggedIn) {
+        Q.Game.User.disconnect();
+      } else {
+        Q.Game.User.connect();
+      }
     });
   },
 
@@ -71,21 +66,22 @@ Q.UI.FBButton = Q.UI.Button.extend("UI.FBButton", {
 }); 
 
 Q.UI.AudioButton = Q.UI.Button.extend("UI.AudioButton", {
-  init: function(p) {
+  init: function(p, callback, defaultProps) {
     var _this = this;
-    this._super(p, {
+    this._super(Q._defaults(p,{
       x: 0,
       y: 0,
       type: Q.SPRITE_UI | Q.SPRITE_DEFAULT,
       sheet: "ui_audio_on",
       keyActionName: "audioMute"
-    });
+    }));
     if (Q.AudioManager.audioMuted) {
       this.p.sheet = "ui_audio_off";
     } else {
       this.p.sheet = "ui_audio_on";
     }
-    return this.on('click', function() {
+
+    this.on('click', function() {
       if (!Q.AudioManager.audioMuted) {
         Q.AudioManager.audioMute();
         _this.p.sheet = "ui_audio_off";
@@ -98,21 +94,22 @@ Q.UI.AudioButton = Q.UI.Button.extend("UI.AudioButton", {
 }); 
 
 Q.UI.MusicButton = Q.UI.Button.extend("UI.MusicButton", {
-  init: function(p) {
+  init: function(p, callback, defaultProps) {
     var _this = this;
-    this._super(p, {
+    this._super(Q._defaults(p,{
       x: 0,
       y: 0,
       type: Q.SPRITE_UI | Q.SPRITE_DEFAULT,
       sheet: "ui_music_on",
       keyActionName: "musicMute"
-    });
+    }));
     if (Q.AudioManager.musicMuted) {
       this.p.sheet = "ui_music_off";
     } else {
       this.p.sheet = "ui_music_on";
     }
-    return this.on('click', function() {
+
+    this.on('click', function() {
       if (!Q.AudioManager.musicMuted) {
         Q.AudioManager.musicMute();
         _this.p.sheet = "ui_music_off";
@@ -204,14 +201,15 @@ Q.UI.HUDScore = Q.UI.Text.extend("Score",{
 });
 
 Q.UI.LevelButton = Q.UI.Button.extend("UI.LevelButton", {
-  init: function(p) {
+  init: function(p, callback, defaultProps) {
     var _this = this;
-    this._super(p, {
-      type: Q.SPRITE_UI | Q.SPRITE_DEFAULT,
+
+    this._super(Q._defaults(p,{
       sheet: "ui_level_button",
       fontColor: "#ffffff",
       font: "400 70px Finger Paint",
-    });
+    }));﻿
+
     this.p.label = this.p.level;
     this.p.sheetW = 344;
     this.p.sheetH = 230;
@@ -221,17 +219,13 @@ Q.UI.LevelButton = Q.UI.Button.extend("UI.LevelButton", {
       this.p.sheet = "ui_level_button_locked";
       this.p.label = false;
     }
-    return this.on('click', function() {
-        if (_this.p.enabled) {
-          if (_this.p.level >= 1) {
-            Q.Game.stageLevel(_this.p.level);
-//            return Game.stageLevel(_this.p.level);
-          } else {
-//            return Game.stageControlsScreen();
-          }
-        } else {
-//          return Game.trackEvent("Level Button", "clicked", "locked");
+ 
+    this.on('click', function() {
+      if (_this.p.enabled) {
+        if (_this.p.level >= 1) {
+          Q.Game.stageLevel(_this.p.level);
         }
+      }
     });
   }
 
